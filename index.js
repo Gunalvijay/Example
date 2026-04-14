ae5d23c6b7ee2c6a082e0bed3d2dbf495a6a2e17
gunal-gnapitech <gunal.s@gnapi.tech>
2026-04-14 12:32:30 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const bodyParser = require("body-parser")
+const app = express();
+
+const books = [
+    {
+        id: 1,
+        name: 'JS Interview',
+        authorname: 'Gunal'
+    },
+    {
+        id: 2,
+        name: 'JS Interview',
+        authorname: 'Abinash'
+    },
+    {
+        id: 3,
+        name: 'JS Interview',
+        authorname: 'Dinesh'
+    }
+];
+
+app.use(bodyParser.json())
+
+app.get('/books', function(req, res) {
+    return res.json({ books })
+});
+
+// Dynamic Path
+app.get('/books/:bookId', function(req, res) {
+    const id = req.params.bookId;
+    const book = books.find((e) => e.id === Number(id));
+    return res.json({ book})
+});
+
+app.listen(8000, () => console.log(`Serer started at PORT 8000`));

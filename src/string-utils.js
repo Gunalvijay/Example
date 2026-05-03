06434235eebfea8cdf4d793bb4e1cb0c8a671d56
gunal-gnapitech <gunal.s@gnapi.tech>
2026-05-03 22:25:40 +0530

    feat(string-utils): add truncate and slugify

--- src/string-utils.js
+++ src/string-utils.js
@@ -1,3 +1,11 @@
 export function capitalise(str) {
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
+
+export function truncate(str, n, suffix = "...") {
+  return str.length > n ? str.slice(0, n) + suffix : str;
+}
+
+export function slugify(str) {
+  return str.toLowerCase().replace(/\s+/g, "-");
+}
